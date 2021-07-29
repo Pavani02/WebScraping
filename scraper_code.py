a8458589d41d12e204e0558c0790037546aa27f8
Pavani02 <69051189+Pavani02@users.noreply.github.com>
2021-07-29 07:52:21 +0530

    Update scraper_code.py

--- scraper_code.py
+++ scraper_code.py
@@ -30,6 +30,23 @@ def product_details(item):
         no_of_reviews=''
     return_value=(description,price,rating,no_of_reviews,product_link)
     return return_value
+def product_details_horizontal_allignment(item):
+    try:
+        description=item.find('span','a-size-base-plus a-color-base a-text-normal').text
+        whole_price=item.find('span','a-price-whole').text
+        fraction_price=item.find('span','a-price-fraction').text
+        price=whole_price+fraction_price
+        product_link='https://www.amazon.com'+item.find('a','a-link-normal').get('href')
+    except AttributeError:
+        return
+    try:
+        rating=item.find('span','a-icon-alt').text
+        no_of_reviews=item.find('span','a-size-base').text
+    except:
+        rating=''
+        no_of_reviews=''
+    return_value=(description,price,rating,no_of_reviews,product_link)
+    return return_value
 def main_func(product_name):
     chromedriver="C:/Drivers/chromedriver_win32/chromedriver.exe" 
     driver=webdriver.Chrome(chromedriver)
@@ -46,8 +63,26 @@ def main_func(product_name):
                 result.append(item_data)
     driver.close()
     return result
+def main_func_horizontal(product_name):
+    chromedriver="C:/Drivers/chromedriver_win32/chromedriver.exe" 
+    driver=webdriver.Chrome(chromedriver)
+    result=[]
+    url=get_url(product_name)
+    for i in range(1,21):
+        new_url=url.format(i)
+        driver.get(new_url)
+        soup=BeautifulSoup(driver.page_source,'html.parser')
+        products=soup.find_all('div',{'data-component-type':'s-search-result'})
+        for item in products:
+            item_data=product_details_horizontal_allignment(item)
+            if(item_data):
+                result.append(item_data)
+    driver.close()
+    return result
 def func(product_name):
     l=main_func(product_name)
+    if(len(l)==0):
+        l=main_func_horizontal(product_name)
     with open('Results.csv','w',newline='',encoding='utf-8') as f:
         writer=csv.writer(f,delimiter=',')
         writer.writerow({'Description','Price','Rating','Number of reviews','Link'})
